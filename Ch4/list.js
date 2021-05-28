ff88342780b6b31fc88a9f059dd937bf3861875a
etgreer91 <etgreer91@gmail.com>
2021-05-27 19:39:43 -1000

    adds exercise solutions for chapter 4

--- Ch4/list.js
+++ Ch4/list.js
@@ -1,19 +1,9 @@
 // write a function that builds up a list structure when given an array as an argument
 const arrayToList = function(arr) {
+   let list = null; 
 
-   let tmp = {
-      value: arr[arr.length - 1],
-      rest: null 
-   };
-
-   let list = {}; 
-
-   for(let i = arr.length - 2; i >= 0; i--) {
-      list = {
-         value: arr[i], 
-         rest: tmp
-      }
-      tmp = list;
+   for(let i = arr.length - 1; i >= 0; i--) {
+      list = {value: arr[i], rest: list};
    }
 
    return list;
@@ -21,39 +11,25 @@ const arrayToList = function(arr) {
 
 // write a function that produces an array from a list
 const listToArray = function(list) {
-   let node = list;
    let array = [];
-   while(node) {
-      array.push(node.value);
-      node = node.rest;    
+   for(let node = list; node; node = node.rest) {
+      array.push(node.value);  
    }
    return array; 
 }
 
 // write a helper function which takes an element and a list and 
 // creates a new list that adds the element to the front of the input list
-const prepend = function(element, list) {
-   return newList = {
-      value: element,
-      rest: list    
-   } 
+const prepend = function(value, list) {
+   return newList = {value, rest: list} 
 }
 
 // write a helper function which takes a list and a number
 // returns the element at the given position in the list or undefined when there is no such element
 const nth = function(list, num) {
-    let element;
-    let count = 0;
-
-    while(list) {
-       element = list.value;
-       if(element === null) return undefined; 
-       if(count === num) {
-          return element;    
-       }
-       list = list.rest;
-       count++; 
-    }
+   if(!list) return undefined;
+   else if(num == 0) return list.value;
+   else return nth(list.rest, num - 1); 
 }
 
-console.log(nth(arrayToList([10, 20, 30]), 3));
+console.log(nth(arrayToList([10, 20, 30]), 5));
--- Ch4/range.js
+++ Ch4/range.js
@@ -1,33 +1,21 @@
-const range = function(start, end, step = 1) {
+const range = function(start, end, step = start < end ? 1 : -1) {
    let result = [];
+
    if(step > 0) {
-      if(start < end) {
-         for(let i = start; i <= end; i += step) {
-            result.push(i);       
-         }    
-         return result; 
-      } else {
-         return "start is greater than end for a positive step value"     
-      }   
+      for(let i = start; i <= end; i += step) result.push(i);        
    } else {
-      if(start > end) {
-         for(let i = start; i >= end; i += step) {
-           result.push(i);       
-         }    
-         return result; 
-      } else {
-         return "start is less than end for a negative step value"     
-      }    
+      for(let i = start; i >= end; i += step) result.push(i);         
    }
 
+   return result;
 }
 
 const sum = function(arr) {
-   let result = 0;
-   for(let i = 0; i < arr.length; i++) {
-      result += arr[i];    
+   let total = 0;
+   for(let value of arr) {
+      total += value;    
    }
-   return result;
+   return total;
 }
 
 console.log(range(5, 2, -1));
